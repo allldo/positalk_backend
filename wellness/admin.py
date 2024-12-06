5a052634fc398f24434d01fbe02c7b6b9ad74696
Allldo <dotan2.maks@mail.ru>
2024-12-06 21:23:12 +0500

    new fields

--- wellness/admin.py
+++ wellness/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from wellness.models import Article, Question, Answer, Test
+from wellness.models import Article, Question, Answer, Test, Block
 
 admin.site.register(Article)
 admin.site.register(Test)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Block)
