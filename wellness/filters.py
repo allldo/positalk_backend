c547a5a266fc0ef1b1ffe4102ff7838ee65646c1
Allldo <dotan2.maks@mail.ru>
2024-12-07 18:06:44 +0500

    searh fixed

--- wellness/filters.py
+++ wellness/filters.py
@@ -1,9 +1,11 @@
+from django.db.models import Q
 from rest_framework.filters import BaseFilterBackend
 
 
 class CaseInsensitiveSearchFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        search_param = request.query_params.get('search', '')
-        if search_param:
-            return queryset.filter(title__icontains=search_param)
+        search_query = request.query_params.get('search', None)
+        if search_query:
+            regex = rf".*{search_query}.*"
+            queryset = queryset.filter(Q(title__iregex=regex))
         return queryset
