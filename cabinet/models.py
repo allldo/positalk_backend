e3bbadaaca4036d8e9e4c4c1e501e7ee5bfde7f4
Allldo <dotan2.maks@mail.ru>
2025-01-06 18:39:52 +0500

    username depleted

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,6 +1,6 @@
 import random
 from django.contrib.auth.models import AbstractUser
-from django.db.models import Model, CharField, DateTimeField, BooleanField
+from django.db.models import Model, CharField, DateTimeField, BooleanField, EmailField, ForeignKey, CASCADE
 from django.utils.timezone import now, timedelta
 
 from cabinet.managers import UserManager
@@ -8,9 +8,10 @@ from cabinet.managers import UserManager
 
 class CustomUser(AbstractUser):
     # REQUIRED_FIELDS = []
-    # USERNAME_FIELD = 'phone_number'
+    username = None
     objects = UserManager()
-    phone_number = CharField(max_length=30, null=True, blank=True, verbose_name='Номер телефона')
+    phone_number = CharField(max_length=30,unique=True, null=True, blank=True, verbose_name='Номер телефона')
+    USERNAME_FIELD = 'phone_number'
 
 
 class PhoneVerification(Model):
@@ -25,3 +26,16 @@ class PhoneVerification(Model):
     @staticmethod
     def generate_code():
         return str(random.randint(100000, 999999))
+
+
+class Survey(Model):
+    user = ForeignKey(CustomUser, on_delete=CASCADE, related_name="profile")
+    nickname = CharField(max_length=225, null=True, blank=True)
+    had_therapy_before = BooleanField(default=False)
+    date_of_birth = CharField(max_length=128, null=True, blank=True)
+    email = EmailField(unique=True, null=True, blank=True)
+    promo_code = CharField(max_length=225, null=True, blank=True)
+    feeling = CharField(max_length=225, null=True, blank=True)
+    relation = CharField(max_length=225, null=True, blank=True)
+    work_study = CharField(max_length=225, null=True, blank=True)
+    life_event = CharField(max_length=225, null=True, blank=True)
