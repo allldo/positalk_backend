a1778d57ab6f3200cc8de1b9e03a398f0ccadd08
Allldo <dotan2.maks@mail.ru>
2025-02-13 16:50:36 +0500

    serializer

--- psy_store/serializers.py
+++ psy_store/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.fields import DecimalField, ListField, CharField
 from rest_framework.relations import StringRelatedField
 from rest_framework.serializers import ModelSerializer
 
-from cabinet.models import PsychologistSurvey, Education
+from cabinet.models import PsychologistSurvey, Education, CustomUser
 from psy_store.models import GiftSession
 from wellness.models import PsychoTopic
 
@@ -27,18 +27,15 @@ class PsychologistsSurveySerializer(ModelSerializer):
     # Для образования передаем JSON-строку
     education_psychologist = CharField(write_only=True, required=False)
     rating = DecimalField(read_only=True, max_digits=2, decimal_places=1)
+    phone_number = CharField(max_length=45, write_only=True)
 
     class Meta:
         model = PsychologistSurvey
         exclude = ['is_approved', 'user']
 
     def create(self, validated_data):
-        request = self.context['request']
-        user = request.user
 
-        # Извлекаем данные для направлений (psycho_topic)
         psycho_topics_data = validated_data.pop('psycho_topic', [])
-        # Если данные пришли не в виде списка, например, как строка, обрабатываем
         if isinstance(psycho_topics_data, str):
             final_topics = [name.strip() for name in psycho_topics_data.split(",") if name.strip()]
         elif isinstance(psycho_topics_data, list):
@@ -53,10 +50,8 @@ class PsychologistsSurveySerializer(ModelSerializer):
         else:
             final_topics = []
 
-        # Создаем или находим объекты PsychoTopic для каждого отдельного названия
         psycho_topics = [PsychoTopic.objects.get_or_create(name=name)[0] for name in final_topics]
 
-        # Обрабатываем образование, которое всегда создается
         education_json = validated_data.pop('education_psychologist', '[]')
         try:
             education_data = json.loads(education_json)
@@ -64,9 +59,9 @@ class PsychologistsSurveySerializer(ModelSerializer):
             education_data = []
         education_instances = [Education.objects.create(**edu) for edu in education_data]
 
-        # Создаем анкету психолога (без ManyToMany полей)
+        user = CustomUser.objects.get_or_create(phone_number=validated_data.get('phone_number'), user_type='psychologist')[0]
+
         survey = PsychologistSurvey.objects.create(user=user, **validated_data)
-        # Устанавливаем связи ManyToMany
         survey.psycho_topic.set(psycho_topics)
         survey.education_psychologist.set(education_instances)
 
