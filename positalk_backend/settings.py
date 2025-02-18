a74986e3c800c9d0060d32a59d9c561ba9455d9a
Allldo <dotan2.maks@mail.ru>
2025-02-19 00:38:35 +0500

    settings

--- positalk_backend/settings.py
+++ positalk_backend/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = '/root/positalk_backend/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
