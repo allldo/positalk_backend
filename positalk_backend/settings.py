d128a9bb14e0591c20780933339600b6d1cf277c
Allldo <dotan2.maks@mail.ru>
2025-02-19 00:17:01 +0500

    config

--- positalk_backend/settings.py
+++ positalk_backend/settings.py
@@ -175,6 +175,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
