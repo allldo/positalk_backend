0552744eb0fb59d8a8c4d100bdf1d3888eceb5cf
Allldo <dotan2.maks@mail.ru>
2025-02-18 18:10:59 +0500

    finakls

--- session/middlewares.py
+++ session/middlewares.py
@@ -3,46 +3,29 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework.authtoken.models import Token
 from django.db import close_old_connections
 from channels.db import database_sync_to_async
-import logging
-
-logger = logging.getLogger(__name__)
 
 class TokenAuthMiddleware(BaseMiddleware):
-    def __init__(self, inner):
-        super().__init__(inner)
-
     async def __call__(self, scope, receive, send):
         close_old_connections()
         scope["user"] = AnonymousUser()
-        headers = dict(scope.get("headers", []))
 
-        # Проверяем заголовок `Sec-WebSocket-Protocol`
-        subprotocol = headers.get(b"sec-websocket-protocol", b"").decode("utf-8")
+        query_string = scope.get("query_string", b"").decode()
+        token_key = self.get_token_from_query(query_string)
 
-        if subprotocol.startswith("Token "):
-            token_key = subprotocol.split("Token ")[1]
+        if token_key:
             user = await self.get_user_by_token(token_key)
-
             if user:
                 scope["user"] = user
-                scope['nickname'] = await database_sync_to_async(user.get_name)()
-                logger.info(f"nickname: {scope['nickname']}")
-            else:
-                logger.warning(f"Invalid token: {token_key}")
-                await send({"type": "websocket.close"})
-                return
 
-        async def send_with_protocol(message):
-            if message["type"] == "websocket.accept":
-                message["subprotocol"] = subprotocol
-            await send(message)
+        return await super().__call__(scope, receive, send)
 
-        return await super().__call__(scope, receive, send_with_protocol)
+    def get_token_from_query(self, query_string):
+        params = dict(param.split("=") for param in query_string.split("&") if "=" in param)
+        return params.get("token")
 
     @database_sync_to_async
     def get_user_by_token(self, token_key):
         try:
-            token = Token.objects.get(key=token_key)
-            return token.user
+            return Token.objects.get(key=token_key).user
         except Token.DoesNotExist:
             return None
