a1433e376739b6d457a23be6697866c245083a1d
Allldo <dotan2.maks@mail.ru>
2025-02-27 19:03:03 +0500

    settings media

--- positalk_backend/settings.py
+++ positalk_backend/settings.py
@@ -166,8 +166,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+MEDIA_URL = 'https://backend.позитолк.рф/media/'
 
-MEDIA_URL = 'media/'
+# MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
